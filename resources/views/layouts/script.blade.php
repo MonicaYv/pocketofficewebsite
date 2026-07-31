fc89c564c2f7cc1353570b687032edc3d560613a
Abhi <109573070+iam-abhijeet@users.noreply.github.com>
2026-07-31 18:36:05 +0530

    SEO related fixed

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,10 +1,9 @@
 <!-- resources/views/layouts/script.blade.php -->
 
-<!-- 1. Global CDN Dependencies (Safe from require errors) -->
- 
+<!-- 1. Global CDN Dependencies loaded after initial paint -->
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 @if(request()->is('contact-us'))
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
@@ -12,7 +11,6 @@
 
 @php
     $viteInputs = [
-        'resources/js/app.js',
         'resources/js/main.js',
         'resources/js/search-box.js',
         'resources/js/search-data.js',
